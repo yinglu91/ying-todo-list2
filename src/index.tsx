27a4e2f090735f9d164deb2ebf592841f02f5c93
Ying Lu <yinglu5374@gmail.com>
2023-10-08 20:05:27 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { ToDoListDesign } from "./screens/ToDoListDesign";
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
+import App from './App'
 
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<ToDoListDesign />);
+const app = document.getElementById('app')
+const root = ReactDOMClient.createRoot(app)
+root.render(<App />)
